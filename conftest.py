fb3340ba72a7769b475ebb51002c66444f8480d4
Dmytro Lunov <dmitriy.lunov@gmail.com>
2025-11-27 13:49:41 +0200

    deleted deprecated fixtures and tests

--- conftest.py
+++ conftest.py
@@ -3,31 +3,6 @@ from modules.api.clients.github import GitHub
 from modules.common.database import Database
 
 
-class User:
-
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
-
-    def create(self):
-        self.name = "Lundi"
-        self.second_name = "Abarth"
-
-    def remove(self):
-        self.name = ""
-        self.second_name = ""
-
-
-@pytest.fixture
-def user():
-    user = User()
-    user.create()
-
-    yield user
-
-    user.remove()
-
-
 @pytest.fixture
 def github_api():
     api = GitHub()
