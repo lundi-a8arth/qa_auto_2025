027bbf9d0ae8dc656cac9ac99803bd48f59d4fbf
Dmytro Lunov <dmitriy.lunov@gmail.com>
2025-12-02 15:35:08 +0200

    tweaked locators and methods in ExceptionPage page object + tweaked tests to work properly after this tweak

--- modules/ui/page_objects/exceptions_page.py
+++ modules/ui/page_objects/exceptions_page.py
@@ -5,6 +5,15 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class ExceptionsPage(BasePage):
     URL = "https://practicetestautomation.com/practice-test-exceptions/"
+    ADD_BTN_LOCATOR = (By.ID, "add_btn")
+    EDIT_1_BTN = (By.CSS_SELECTOR, "#row1 .Edit")
+    EDIT_2_BTN = (By.CSS_SELECTOR, "#row2 .Edit")
+    SAVE_1_BTN = (By.CSS_SELECTOR, "#row1 .Save")
+    SAVE_2_BTN = (By.CSS_SELECTOR, "#row2 .Save")
+    REMOVE_BTN = (By.ID, "remove_btn")
+    INPUT_FIELD_1 = (By.CSS_SELECTOR, "#row1 .input-field")
+    INPUT_FIELD_2 = (By.CSS_SELECTOR, "#row2 .input-field")
+    INSTRUCTIONS_ELEM = (By.ID, "instructions")
 
     def __init__(self):
         super().__init__()
@@ -13,79 +22,67 @@ class ExceptionsPage(BasePage):
         self.driver.get(ExceptionsPage.URL)
 
     def press_add_btn(self):
-        add_btn = self.wait.until(EC.element_to_be_clickable((By.ID, "add_btn")))
-        add_btn.click()
+        self.wait.until(
+            EC.element_to_be_clickable((ExceptionsPage.ADD_BTN_LOCATOR))
+        ).click()
 
     def press_edit_1_btn(self):
-        edit_1_btn = self.wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#row1 .Edit"))
-        )
-        edit_1_btn.click()
+        self.wait.until(EC.element_to_be_clickable((ExceptionsPage.EDIT_1_BTN))).click()
 
     def press_edit_2_btn(self):
-        edit_2_btn = self.wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#row2 .Edit"))
-        )
-        edit_2_btn.click()
+        self.wait.until(EC.element_to_be_clickable((ExceptionsPage.EDIT_2_BTN))).click()
 
     def press_save_1_btn(self):
-        save_1_btn = self.driver.find_element(By.CSS_SELECTOR, "#row1 .Save")
-        save_1_btn.click()
+        self.wait.until(EC.element_to_be_clickable((ExceptionsPage.SAVE_1_BTN))).click()
 
     def press_save_2_bnt(self):
-        save_2_btn = self.driver.find_element(By.CSS_SELECTOR, "#row2 .Save")
-        save_2_btn.click()
+        self.wait.until(EC.element_to_be_clickable((ExceptionsPage.SAVE_2_BTN))).click()
 
     def press_save_bnt_by_name(self):
         save_btn = self.driver.find_element(By.NAME, "Save")
         save_btn.click()
 
     def press_remove_btn(self):
-        remove_btn = self.driver.find_element(By.ID, "remove_btn")
-        remove_btn.click()
+        self.wait.until(EC.element_to_be_clickable((ExceptionsPage.REMOVE_BTN))).click()
 
     def send_text_to_input_field_1(self, text):
-        input_field_1 = self.wait.until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#row1 .input-field"))
-        )
-        input_field_1.send_keys(text)
+        self.wait.until(
+            EC.visibility_of_element_located((ExceptionsPage.INPUT_FIELD_1))
+        ).send_keys(text)
 
     def send_text_to_input_field_2(self, text):
-        input_field_2 = self.wait.until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#row2 .input-field"))
-        )
-        input_field_2.send_keys(text)
+        self.wait.until(
+            EC.visibility_of_element_located((ExceptionsPage.INPUT_FIELD_2))
+        ).send_keys(text)
 
     def clear_input_field_1(self):
-        input_field_1 = self.wait.until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#row1 .input-field"))
-        )
-        input_field_1.clear()
+        self.wait.until(
+            EC.visibility_of_element_located((ExceptionsPage.INPUT_FIELD_1))
+        ).clear()
 
     def clear_input_field_2(self):
-        input_field_2 = self.wait.until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#row2 .input-field"))
-        )
-        input_field_2.clear()
+        self.wait.until(
+            EC.visibility_of_element_located((ExceptionsPage.INPUT_FIELD_2))
+        ).clear()
 
     def input_field_2_is_displayed_without_wait(self):
-        input_field_2 = self.driver.find_element(By.CSS_SELECTOR, "#row2 .input-field")
+        input_field_2 = self.driver.find_element(*ExceptionsPage.INPUT_FIELD_2)
         return input_field_2.is_displayed()
 
     def input_field_2_is_displayed_with_wait(self):
         input_field_2 = self.wait.until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "#row2 .input-field"))
+            EC.element_to_be_clickable((ExceptionsPage.INPUT_FIELD_2))
         )
         return input_field_2.is_displayed()
 
     def get_instructions_elem(self):
         instructions_elem = self.wait.until(
-            EC.visibility_of_element_located((By.ID, "instructions"))
+            EC.visibility_of_element_located((ExceptionsPage.INSTRUCTIONS_ELEM))
         )
         return instructions_elem
 
     def get_input_field_2_elem(self):
         input_field_2_elem = self.wait.until(
-            EC.visibility_of_element_located((By.ID, "instructions"))
+            EC.visibility_of_element_located((ExceptionsPage.INSTRUCTIONS_ELEM))
         )
         input_field_2_elem.is_displayed()
--- tests/ui/test_ui_exceptions_page.py
+++ tests/ui/test_ui_exceptions_page.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import (
 import pytest
 
 
-@pytest.mark.ui
+@pytest.mark.ui_exceptions
 def test_no_such_elem_exception(exceptions_page):
     exceptions_page.go_to()
     exceptions_page.press_add_btn()
@@ -16,7 +16,7 @@ def test_no_such_elem_exception(exceptions_page):
         exceptions_page.input_field_2_is_displayed_without_wait()
 
 
-@pytest.mark.ui
+@pytest.mark.ui_exceptions
 def test_elem_not_interactable_exception(exceptions_page):
     exceptions_page.go_to()
     exceptions_page.press_add_btn()
@@ -25,14 +25,14 @@ def test_elem_not_interactable_exception(exceptions_page):
         exceptions_page.press_save_bnt_by_name()
 
 
-@pytest.mark.ui
+@pytest.mark.ui_exceptions
 def test_invalid_elem_state_exception(exceptions_page):
     exceptions_page.go_to()
     with pytest.raises(InvalidElementStateException):
         exceptions_page.clear_input_field_1()
 
 
-@pytest.mark.ui
+@pytest.mark.ui_exceptions
 def test_stale_elem_reference_exception(exceptions_page):
     exceptions_page.go_to()
     element = exceptions_page.get_instructions_elem()
@@ -42,7 +42,7 @@ def test_stale_elem_reference_exception(exceptions_page):
         element.is_displayed()
 
 
-@pytest.mark.ui
+@pytest.mark.ui_exceptions
 def test_timeout_exception(exceptions_page):
     exceptions_page.go_to()
     exceptions_page.press_add_btn()
